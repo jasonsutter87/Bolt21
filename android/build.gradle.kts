721d98ff9d0d7e2a3bf09664a0729e86d6dffda1
Jason Sutter <jasonsutter87@gmail.com>
2025-12-28 09:23:35 -0800

    Fix Android build: AGP 8.9.1, Kotlin 2.1.0, namespace workaround for ldk_node

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,22 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Fix namespace for older Flutter plugins (ldk_node, etc.) on AGP 8+
+subprojects {
+    plugins.withId("com.android.library") {
+        val android = extensions.getByType(com.android.build.gradle.LibraryExtension::class.java)
+        if (android.namespace == null) {
+            val manifest = file("src/main/AndroidManifest.xml")
+            if (manifest.exists()) {
+                val packageName = groovy.xml.XmlSlurper().parse(manifest).getProperty("@package")?.toString()
+                if (!packageName.isNullOrEmpty()) {
+                    android.namespace = packageName
+                }
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
